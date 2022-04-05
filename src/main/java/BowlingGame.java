69f4e683d9350f104bcc5a36158faa08e7f9939f
sabo <sabo@jihyes-mbp.home>
2022-04-05 22:27:57 +0100

    Add test canScoreGutterGame for getting game score when the total score is 0

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,4 +2,8 @@ public class BowlingGame {
     public void roll(int pinsDown) {
 
     }
+
+    public int score() {
+        return 0;
+    }
 }
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -5,7 +5,7 @@ public class BowlingTest {
 
     @Test
     public void canMakeGame() {
-        BowlingGame game = new BowlingGame();
+        BowlingGame bowlingGame = new BowlingGame();
     }
 
     @Test
@@ -14,5 +14,20 @@ public class BowlingTest {
         game.roll(0);
     }
 
+    @Test
+    // When the ball is fall to the side gutter and can't knock down any pin, score 0
+    // Test if game can make the score when all 20 rolled balls fall into the gutter and total score is 0
+    public void canScoreGutterGame() {
+        BowlingGame game = new BowlingGame();
+        int expected = 0;
+
+        for(int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        int actual = game.score();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 
 }
