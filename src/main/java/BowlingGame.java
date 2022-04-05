b9854bbfef02023db9555340d609b0d7c0164436
sabo <sabo@jihyes-mbp.home>
2022-04-05 23:13:30 +0100

    add Test canScoreSpareFollowedByThree and add spare scoring logic in BowlingGame class

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,12 +1,29 @@
 public class BowlingGame {
+    private int roll = 0;
+
+    // For storing the whole pinsDown of the each roll
+    private int[] rolls = new int[21]; // the most rolls you can have in the game. (no Strike and one Spare in the last Frame)
+
     private int score = 0;
 
     public void roll(int pinsDown) {
-        score += pinsDown;
-
+        // Store the pinsDown number in the array [current roll] and increase the roll
+        rolls[roll++] = pinsDown;
     }
 
     public int score() {
+        int score = 0;
+        int cursor = 0;
+
+        for(int frame = 0; frame < 10; frame++) {
+            if(rolls[cursor] + rolls[cursor + 1] == 10) { // Spare
+                score = score + 10 + rolls[cursor + 2];
+                cursor += 2; // move cursor to the next Frame
+            } else {
+                score = score + rolls[cursor] + rolls[cursor + 1];
+                cursor += 2; // move cursor to the next Frame
+            }
+        }
         return score;
     }
 }
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -25,10 +25,22 @@ public class BowlingTest {
     }
 
     @Test
-    //
+    // Test if game can make the score when all 20 rolled balls knock down 1 pin.
     public void canScoreGameWhenAllRollIsOne() {
         roll(20, 1);
         Assertions.assertEquals(20, game.score());
+    }
+
+    @Test
+    // Test for when spare and new next roll is 3
+    // ex) First Roll 5, Second Roll 5 (Spare) and the next Roll 3
+    // The First Frame is 13 (10 + 3) and the next Frame's first roll is 3 so total score is 16
+    public void canScoreSpareFollowedByThree() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        roll(17, 0); // rest are all 0
+        Assertions.assertEquals(16, game.score());
     }
 
 }
